d34972039bf64c06127f51e45ae05d898c6fce88
OSBJE <osbj0001rstud.kea.dk>
2024-10-12 13:08:02 +0200

    1.step test conn obj

---
+++ src/main/java/Turistguide/model/DBConnection.java
@@ -0,0 +1,30 @@
+package Turistguide.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private String URL;
+    private String Admin;
+    private String Password;
+
+    public DBConnection(String URL, String Admin, String Password){
+
+    }
+
+    public Connection getConnection(String URL, String Admin, String Password){
+        Connection conn = null;
+
+
+        try {
+            Connection contemp = DriverManager.getConnection(URL,Admin,Password);
+            conn = contemp;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return conn;
+    }
+}
