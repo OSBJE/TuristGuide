8cc8b2ada05bf17f6bcd0440d4b0c1cc3690005f
OSBJE <osbj0001rstud.kea.dk>
2024-10-21 10:52:20 +0200

    not working with h2 database

--- src/test/java/Turistguide/TuristGuideApplicationTests.java
+++ src/test/java/Turistguide/TuristGuideApplicationTests.java
@@ -1,10 +1,27 @@
 package Turistguide;
 
+import Turistguide.model.TouristAttraction;
+import Turistguide.repository.TouristRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@ActiveProfiles("h2")
 class TuristGuideApplicationTests {
+
+	@Autowired
+	TouristRepository repository;
+
+	@Test
+	void findTouristAttraction(){
+		TouristAttraction objFound = repository.getAttractionDb("Den Lille havfrue");
+		assertEquals("Den Lille havfrue", objFound.getName());
+	}
+
 
 	@Test
 	void contextLoads() {
