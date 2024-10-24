1fdf942f294afb4704a00941686461244f4f02e5
OSBJE <osbj0001rstud.kea.dk>
2024-10-24 10:56:31 +0200

    osbjeTest in main

--- src/test/java/Turistguide/repository/TouristRepositoryTest.java
+++ src/test/java/Turistguide/repository/TouristRepositoryTest.java
@@ -6,9 +6,12 @@ import Turistguide.repository.TouristRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Repository;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ActiveProfiles("h2")
@@ -29,12 +32,22 @@ public class TouristRepositoryTest {
 
     @Test
     void deleteAttraction(){
+        TouristAttraction objFound  = repository.getAttractionDb("Den Lille havfrue");
+        assertEquals("Den Lille havfrue", objFound.getName());
 
+        repository.deleteAttraction("Den Lille havfrue");
+        TouristAttraction objFound2  = repository.getAttractionDb("Den Lille havfrue");
+        assertNull(objFound2);
+
+        TouristAttraction controlObj = repository.getAttractionDb("Rundetårn");
+        assertNotNull(controlObj);
     }
 
     @Test
     void getListOfCities(){
-
+        List<String> list = repository.getListOfCities();
+        assertTrue(list.contains("København"));
+        assertFalse(list.contains("Vordingborg"));
     }
 
 }
